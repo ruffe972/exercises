45c69372d14986fd509565ddbda535b9bbf045a9
Ivan Ershov <ivanershov51@gmail.com>
2020-12-22 22:44:38 +0300

    Reactive calculator: copy starter

---
+++ reactive_calculator/server/src/main/java/ruffe972/reactive_calculator/ReactiveCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ruffe972.reactive_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCalculatorApplication.class, args);
+	}
+
+}
---
+++ reactive_calculator/server/src/test/java/ruffe972/reactive_calculator/ReactiveCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ruffe972.reactive_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
