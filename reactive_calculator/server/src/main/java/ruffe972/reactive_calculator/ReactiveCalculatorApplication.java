3f86b575b323cbefb02d594d577874531dc8165b
Ivan Ershov <ivanershov51@gmail.com>
2020-12-22 22:49:42 +0300

    Remove tests and whitespace

--- reactive_calculator/server/src/main/java/ruffe972/reactive_calculator/ReactiveCalculatorApplication.java
+++ reactive_calculator/server/src/main/java/ruffe972/reactive_calculator/ReactiveCalculatorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactiveCalculatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveCalculatorApplication.class, args);
 	}
-
 }
